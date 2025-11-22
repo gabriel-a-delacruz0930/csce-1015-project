224cc7d7dc9fd8379295449e7cb0f2fd97b40225
TaylorJaffke <taylorjaffke@my.unt.edu>
2025-11-21 21:47:43 -0600

    Add files via upload

---
+++ feature/infostorage_alpha.py
@@ -0,0 +1,18 @@
+import os
+
+#not finished, might not compile properly.
+def fishStore():
+    species = input("Enter fish name: ")
+    color = input("Enter fish color: ")
+    location = input("Enter fish location: ")
+    weight = input("Enter fish weight: ")
+    date_caught = input("Enter date caught: ")
+
+    print("\nFish Information: \n")
+    print (f"Fish Name: {species} \n")
+    print (f"Fish Color: {color} \n")
+    print (f"Fish Location: {location} \n")
+    print (f"Fish Weight: {weight} \n")
+    print (f"Date Caught: {date_caught} \n")
+
+
