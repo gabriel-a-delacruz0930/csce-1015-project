374b32959ebe74462cfe79b5480956aca24bcee3
TaylorJaffke <taylorjaffke@my.unt.edu>
2025-12-08 16:35:42 -0600

    Update infostorage_alpha.py
    Should be final code, maybe?

--- feature/infostorage_alpha.py
+++ feature/infostorage_alpha.py
@@ -1,18 +1,26 @@
 import os
 
-#not finished, might not compile properly.
+#Simple program to catalouge fish info
+#Only stores basic information in strings.
+
 def fishStore():
+    print("\n[======] Fish Information Storage Program: [======] \n")
+
+    print("\n[======] Enter Info: [======]")
+
     species = input("Enter fish name: ")
     color = input("Enter fish color: ")
     location = input("Enter fish location: ")
-    weight = input("Enter fish weight: ")
-    date_caught = input("Enter date caught: ")
+    weight = input("Enter fish weight (in lbs): ")
+    date_caught = input("Enter date caught (MM/DD/YYYY format): ")
 
-    print("\nFish Information: \n")
+    print(f"\n {species} has been catalogued sucessfully!\n")
+    
+    
+    print("\n[======] Stored Info: [======] \n")
     print (f"Fish Name: {species} \n")
     print (f"Fish Color: {color} \n")
     print (f"Fish Location: {location} \n")
-    print (f"Fish Weight: {weight} \n")
+    print (f"Fish Weight: {weight} lbs\n")
     print (f"Date Caught: {date_caught} \n")
 fishStore()
-
